41c8ce22a440d0e18684c32a6df13c4fa461e5b4
Kapil Gorve <kapilsg186@gmail.com>
2021-05-11 08:02:44 +0530

    update url

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -19,13 +19,13 @@ exports.handler = async function(event, ctx) {
         // secure: true,
         custom_pre_function: {
           function_type: "remote",
-          source: `https://friendly-heyrovsky-86d9f9.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+          source: `https://ogurl.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
             queryStringParameters
           )}`
         }
       }
     );
-    console.log(`https://friendly-heyrovsky-86d9f9.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+    console.log(`https://ogurl.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
             queryStringParameters
           )}`)
     return {
